9f31d1f5038a0c9ee64476b42fbe09145e525889
renadkfakhoury <renadkazem2002@gmail.com>
2021-06-20 12:28:48 +0300

    first commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,90 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    var scaffold = Scaffold(
+        body: Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.width,
+      color: Colors.pink[400],
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Column(
+          children: [
+            Row(
+              //the first row
+              children: [
+                FlatButton(
+                    onPressed: () {},
+                    child: Text('Home', style: TextStyle(color: Colors.white))),
+                FlatButton(
+                    onPressed: () {},
+                    child: Text('file', style: TextStyle(color: Colors.white))),
+                FlatButton(
+                    onPressed: () {},
+                    child:
+                        Text('about', style: TextStyle(color: Colors.white))),
+                FlatButton(
+                    onPressed: () {},
+                    child: Text('help', style: TextStyle(color: Colors.white))),
+                Spacer(),
+                Text(
+                  "FreeLarn login  ",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+              
+               Expanded(child:  Padding(
+                 padding: const EdgeInsets.only(top: 25),
+                 child: Container(child: Image.asset("images/cloud.jpg" ,width: 530, height: 320 , )),
+               ),),
+                Expanded(child: Container(
+                  child: Column(
+                    crossAxisAlignment:CrossAxisAlignment.start ,
+                    children: [
+                    Text(
+                      " Lorem Ipsum, giving  on  its origins  giving  ",
+                      style:TextStyle(color: Colors.white , fontSize: 40 ),
+                    ),
+                     SizedBox(height: 10,),
+                     Text(
+                      " Lorem Ipsum, giving information on its origins,giving information on its origins, giving information on its origins giving information on its origins, giving information on its origins",
+                      style:TextStyle(color: Colors.white , fontSize: 10 ),
+                      ),
+                       SizedBox(height: 10,),
+                      Container(
+                        
+                        decoration: BoxDecoration(
+                          color: Colors.pink[500],
+                          borderRadius: BorderRadius.circular(20),
+                      ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 10, left: 6 ,top:4 ),
+                          child: Text("read More", style:TextStyle(fontSize:10 ,color:Colors.white )
+                          ),
+                        )),
+                  ]),
+                )),
+              ],
+            ),
+            Row(),
+          ],
+        ),
+      ),
+    ));
+    return scaffold;
+  }
+}
+
+class Cairo {}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'HomePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "project123",
+      debugShowCheckedModeBanner: false,
+      
+      home:HomePage(),
+    );
+    
+  }
+}
